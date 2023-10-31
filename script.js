f15e3293e3ac97e8f7a554e2fc3bb609dba66315
GuMilano <gustavo.milanosilva@outlook.com>
2023-10-31 13:06:58 -0300

    Projeto Guiado Rocketseat

---
+++ script.js
@@ -0,0 +1,34 @@
+function toggleMode(){
+    const html = document.documentElement
+        /*if(html.classList.contains('light')) {
+     html.classList.remove('light')   
+    }else{
+        html.classList.add('light')
+    } 
+    Seria o mesmo que: */
+        html.classList.toggle('light')
+
+    //pegar a tag image - mesmo seletor css
+    const img = document.querySelector("#profile img")
+     //se tiver light mode, adicionar a imagem light  
+     if(html.classList.contains('light')) {
+     //substituir a imagem
+        img.setAttribute('src','./assets/avatar-light.png')
+     } else{
+    // se tiver sem light mode, manter  a imagem normal
+        img.setAttribute('src','./assets/avatar.png')
+
+     } 
+    
+
+
+
+
+    
+
+
+
+}
+
+
+
